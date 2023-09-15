fa143af1fa5fd1bbd8acc529f576761700d0fe38
Vitor Rodarte <vitor@alude.com.br>
2023-09-14 23:16:06 -0300

    Multiple streamer collect

--- src/twitch_scraper/main.py
+++ src/twitch_scraper/main.py
@@ -8,28 +8,30 @@ print("Starting scrapper...")
 twitch_comment_scrapper = TwitchVideoCommentsScraper()
 video_list = TwitchVideoList()
 
-streamer_login = input("Type the streamer login that should be crawled: ")
+streamer_logins = input("Type the streamer login that should be crawled: ")
 
-videos = video_list.get_video_list(streamer_login)
+logins = streamer_logins.split(",")
 
+for login in logins:
+    videos = video_list.get_video_list(login)
 
-streamer_file_path = f"out/{streamer_login}.json"
+    streamer_file_path = f"out/{login}.json"
 
-os.makedirs(os.path.dirname(streamer_file_path), exist_ok=True)
+    os.makedirs(os.path.dirname(streamer_file_path), exist_ok=True)
 
-with open(streamer_file_path, "w") as outfile:
-    outfile.write(json.dumps(videos))
+    with open(streamer_file_path, "w") as outfile:
+        outfile.write(json.dumps(videos))
 
-for video in videos["videos"]:
-    print(f"Scrapping video {video['id']}...")
+    for video in videos["videos"]:
+        print(f"Scrapping video {video['id']}...")
 
-    comments = twitch_comment_scrapper.get_video_comments(video["id"])
+        comments = twitch_comment_scrapper.get_video_comments(video["id"])
 
-    print("Writing result...")
+        print("Writing result...")
 
-    video_file_path = f"out/{streamer_login}/{video['id']}.json"
+        video_file_path = f"out/{login}/{video['id']}.json"
 
-    os.makedirs(os.path.dirname(video_file_path), exist_ok=True)
+        os.makedirs(os.path.dirname(video_file_path), exist_ok=True)
 
-    with open(video_file_path, "w") as outfile:
-        outfile.write(json.dumps(comments))
+        with open(video_file_path, "w") as outfile:
+            outfile.write(json.dumps(comments))
